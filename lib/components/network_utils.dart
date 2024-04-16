0b19f5af4999d59cd855e5702948727843d63af7
dangphuc2470 <dangnguyenhoangphuc1@gmail.com>
2024-04-16 22:10:24 +0700

    Location search

---
+++ lib/components/network_utils.dart
@@ -0,0 +1,11 @@
+import 'package:http/http.dart' as http;
+
+class NetworkUtils {
+  static Future<String?> fetchUrl(Uri uri, {Map<String, String>? headers}) async {
+    final response = await http.get(uri, headers: headers);
+    if (response.statusCode == 200) {
+      return response.body;
+    }
+    return null;
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:voyageventure/AddressConverter/my_converter.dart';
 import 'package:voyageventure/MyHomeScreen/my_home_screen.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:voyageventure/MySearchBar/my_search_bar.dart';
+import 'package:voyageventure/MyLocationSearch/my_location_search.dart';
+
 void main() async {
   await dotenv.load();
   runApp(MyApp());
@@ -21,7 +22,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: MySearchBar(),
+      home: SearchLocationScreen(),
     );
   }
 }
