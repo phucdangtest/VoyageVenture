f60e689e551fab456a6de83620bcbe9138c235b3
dangphuc2470 <dangnguyenhoangphuc1@gmail.com>
2024-04-29 16:22:02 +0700

    Bottom sheet Animation

--- lib/MyHomeScreen/my_home_screen.dart
+++ lib/MyHomeScreen/my_home_screen.dart
@@ -5,6 +5,7 @@ import 'dart:math';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:geolocator/geolocator.dart';
@@ -25,9 +26,16 @@ class MyHomeScreen extends StatefulWidget {
   State<MyHomeScreen> createState() => _MyHomeScreenState();
 }
 
-class _MyHomeScreenState extends State<MyHomeScreen> {
+class _MyHomeScreenState extends State<MyHomeScreen>
+    with SingleTickerProviderStateMixin {
   final Completer<GoogleMapController> _controller = Completer();
   ScrollController _scrollController = ScrollController();
+  DraggableScrollableController? _DragableController =
+      DraggableScrollableController();
+  double? bottomSheetTop;
+  late AnimationController _animationController;
+  late Animation<double> moveAnimation;
+
   Future<List<LatLng>?> polylinePoints = Future.value(null);
   static const CameraPosition _initialCameraPosition = CameraPosition(
     target: LatLng(10.7981542, 106.6614047),
@@ -54,100 +62,157 @@ class _MyHomeScreenState extends State<MyHomeScreen> {
   @override
   void initState() {
     super.initState();
+    _animationController = AnimationController(
+      duration: const Duration(seconds: 5),
+      vsync: this,
+    );
+
+    moveAnimation = CurvedAnimation(
+      parent: _animationController,
+      curve: Curves.fastOutSlowIn,
+    );
     //myMarker.addAll(markerList);
   }
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      // Transparent status bar
+      statusBarIconBrightness: Brightness.dark,
+      // Dark icons
+      systemNavigationBarColor: Colors.transparent,
+      // Transparent navigation bar
+      systemNavigationBarIconBrightness: Brightness.dark, // Dark icons
+    ));
     return Scaffold(
-        body: Stack(
-          children: <Widget>[
-            // Container(
-            //   decoration: BoxDecoration(
-            //     color: Colors.black,
+      body: Stack(
+        children: <Widget>[
+          // Container(
+          //   decoration: BoxDecoration(
+          //     color: Colors.black,
+          //   ),
+          // ),
+          //Todo: Uncomment Google Map
+          GoogleMap(
+            initialCameraPosition: _initialCameraPosition,
+            mapType: MapType.normal,
+            myLocationEnabled: true,
+            //markers: Set.from(myMarker),
+            onMapCreated: (GoogleMapController controller) {
+              _controller.complete(controller);
+            },
+            polylines: {if (route != null) route!},
+            zoomControlsEnabled: false,
+            // markers: {
+            //   Marker(
+            //     markerId: const MarkerId('marker_1'),
+            //     position: const LatLng(10.7981542, 106.6614047),
+            //     infoWindow: const InfoWindow(
+            //       title: 'Marker 1',
+            //       snippet: '5 Star Rating',
+            //     ),
+            //     icon: BitmapDescriptor.defaultMarkerWithHue(
+            //         BitmapDescriptor.hueViolet),
             //   ),
-            // ),
-            //Todo: Uncomment Google Map
-              GoogleMap(
-              initialCameraPosition: _initialCameraPosition,
-              mapType: MapType.normal,
-              myLocationEnabled: true,
-              //markers: Set.from(myMarker),
-              onMapCreated: (GoogleMapController controller) {
-                _controller.complete(controller);
-              },
-                polylines: {
-                  if (route != null) route!
-                },
-              zoomControlsEnabled: false,
-              markers: {
-                Marker(
-                  markerId: const MarkerId('marker_1'),
-                  position: const LatLng(10.7981542, 106.6614047),
-                  infoWindow: const InfoWindow(
-                    title: 'Marker 1',
-                    snippet: '5 Star Rating',
-                  ),
-                  icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueViolet),
+            //   Marker(
+            //     markerId: const MarkerId('marker_2'),
+            //     position: const LatLng(10.9243059, 106.8155907),
+            //     infoWindow: const InfoWindow(
+            //       title: 'Marker 2',
+            //       snippet: '4 Star Rating',
+            //     ),
+            //     icon: BitmapDescriptor.defaultMarkerWithHue(
+            //         BitmapDescriptor.hueViolet),
+            //   ),
+            // }
+          ),
+
+          AnimatedPositioned(
+            duration: Duration(milliseconds: 500),
+            curve: Curves.fastOutSlowIn,
+            bottom: (bottomSheetTop == null)
+                ? MediaQuery.of(context).size.height * 20 / 100
+                : bottomSheetTop,
+            right: 10,
+            child: Column(
+              children: [
+                FloatingActionButton(
+                  onPressed: () {
+                    // Handle button press
+                  },
+                  child: Icon(Icons.add),
                 ),
-                Marker(
-                  markerId: const MarkerId('marker_2'),
-                  position: const LatLng(10.9243059, 106.8155907),
-                  infoWindow: const InfoWindow(
-                    title: 'Marker 2',
-                    snippet: '4 Star Rating',
-                  ),
-                  icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueViolet),
+                // Add more widgets here that you want to move with the sheet
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.red,
                 ),
-              }
+                Text('Hello World'),
+              ],
             ),
-            DraggableScrollableSheet(
-             // initialChildSize: 0.2,
-             initialChildSize: 0.8,
-              minChildSize: 0.1,
-              maxChildSize: 1.0,
-              builder:
-                  (BuildContext context, ScrollController scrollController) {
-                return ClipRRect(
-                  borderRadius: const BorderRadius.only(
-                    topLeft: Radius.circular(24.0),
-                    topRight: Radius.circular(24.0),
-                  ),
-                  child: Container(
-                    color: Colors.white,
-                    child: SingleChildScrollView(
-                      controller: scrollController,
-                      child: Column(children: <Widget>[
-                        Pill(),
-                        LocationSearchScreen_(controller: _scrollController),
-                        BottomSheetComponient_(controller: _scrollController),
-                      ]),
-                    ),
-                  ),
-                );
+          ),
+
+          NotificationListener<ScrollNotification>(
+              onNotification: (ScrollNotification scrollInfo) {
+                setState(() {
+                  logWithTab(
+                      "Change from " +
+                          bottomSheetTop.toString() +
+                          "to" +
+                          _DragableController!.pixels.toString(),
+                      tag: "MyHomeScreen");
+                  bottomSheetTop = _DragableController!.pixels;
+                });
+
+                // logWithTab(scrollInfo.toString(), tag: "MyHomeScreen");
+                // if (scrollInfo is ScrollEndNotification) {
+                //   double pixelValue = scrollInfo.metrics.pixels;
+                //   logWithTab('pixel value: ${scrollInfo.metrics.pixels}', tag: "MyHomeScreen1");// In ra giá trị pixel
+                //   logWithTab(_scrollController.position.maxScrollExtent.toString(), tag: "MyHomeScreen2");// In ra giá trị pixel
+                // }
+                //   setState(() {
+                //     _fabPosition = _fabPosition + 10;
+                //     //_fabPosition = scrollInfo.metrics.pixels;
+                //     logWithTab(_fabPosition.toString(),
+                //         tag: "MyHomeScreen");
+                //   });
+
+                return true;
               },
-            )
-          ],
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () async {
-            polylinePoints = computeRoutes(from: _airPort, to: _dormitory);
-            polylinePoints.then((value) {
-              setState(() {
-                if (value != null) {
-                  route = Polyline(
-                    polylineId: const PolylineId('route'),
-                    color: Colors.green,
-                    points: value,
-                    width: 5,
+              child: DraggableScrollableSheet(
+                controller: _DragableController,
+                // initialChildSize: 0.2,
+
+                initialChildSize: 0.2,
+                minChildSize: 0.1,
+                maxChildSize: 1.0,
+                builder:
+                    (BuildContext context, ScrollController scrollController) {
+                  return ClipRRect(
+                    borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(24.0),
+                      topRight: Radius.circular(24.0),
+                    ),
+                    child: Container(
+                      color: Colors.white,
+                      child: SingleChildScrollView(
+                        primary: false,
+                        controller: scrollController,
+                        child: Column(children: <Widget>[
+                          Pill(),
+                          LocationSearchScreen_(controller: _scrollController),
+                          BottomSheetComponient_(controller: _scrollController),
+                        ]),
+                      ),
+                    ),
                   );
-                }
-              });
-            });
-            //(CameraUpdate.newCameraPosition(_initialCameraPosition ));
-          },
-          child: const Icon(Icons.my_location_rounded),
-        ));
+                },
+              ))
+        ],
+      ),
+    );
   }
 }
 
